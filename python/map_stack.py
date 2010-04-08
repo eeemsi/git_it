45a88c9d3a8cde3b28d1f2cc4d8cda7dd1c60876
me <me@anywhere.net>
2010-04-09 01:07:23 +0200

    new file:   python/map_stack.py 	new file:   python/stack.py

---
+++ python/map_stack.py
@@ -0,0 +1,9 @@
+"""
+build a class map where a map needs a vote in integer between 1 and 5
+
+enter methods and sorting comes later
+"""
+map_stack = []
+map_stack.append(raw_input("enter first mapname\n"))
+
+print map_stack
