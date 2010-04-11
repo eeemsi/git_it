dfc2774d2fac5233eb87dd02a905f5b5c6723303
me <me@anywhere.net>
2010-04-11 17:54:06 +0200

    modified:   update_cloned_git.sh

--- update_cloned_git.sh
+++ update_cloned_git.sh
@@ -3,7 +3,7 @@ ARRAY=`ls /home/msi/cloned_git/`
 echo "$ARRAY"
 
 for i in $ARRAY; do
-	if [ -d "/home/msi/cloned_git/$i"]; then
+	if [ -d "/home/msi/cloned_git/$i" ]; then
 	cd /home/msi/cloned_git/$i && git pull;
 	fi
 done
