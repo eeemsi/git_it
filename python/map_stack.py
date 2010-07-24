d9cf9b078383a285794ab7cb2d334f32f930b96f
me <me@anywhere.net>
2010-07-24 12:40:49 +0200

    some old files removed and added modified i3-config

--- python/map_stack.py
+++
@@ -1,9 +0,0 @@
-"""
-build a class map where a map needs a vote in integer between 1 and 5
-
-enter methods and sorting comes later
-"""
-map_stack = []
-map_stack.append(raw_input("enter first mapname\n"))
-
-print map_stack
