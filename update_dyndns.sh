6caa7f192115550bde5e291b2b66d75147c0eaaf
eeemsi <eeemsi@gmail.com>
2012-01-06 00:36:05 +0100

    sometimes you want to force a dyndns update ;)

---
+++ update_dyndns.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+IP=`ifconfig eth0.1 | sed -n '/dr:/{;s/.*dr:\([0-9.]\+\) .*/\1/;p;}'`
+URL="http://username:passwd@members.dyndns.org/nic/update?hostname=bl4.dyndns.org&myip=$cip&wildcard=NOCHG"
+
+curl $URL > /dev/null
+exit 0
