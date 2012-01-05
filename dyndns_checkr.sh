fd199b37debcbcd5e0f0a446fdbe43261fc9c835
eeemsi <eeemsi@gmail.com>
2012-01-06 00:43:21 +0100

    added a usefull comment and renamed the file

--- dyndns_checkr.sh
+++ dyndns_checkr.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# hint: put this into crontab
+# */15 * * * * /bin/dyndns_checkr.sh > /dev/null
+# 0 1 * * 1 /bin/update_dns.sh > /dev/null
+
 cip=`ifconfig eth0.1 | sed -n '/dr:/{;s/.*dr:\([0-9.]\+\) .*/\1/;p;}'`
 lip=`nslookup bl4.dyndns.org | tail -2 | grep Address | awk '{print $3}'`
 
