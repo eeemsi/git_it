716c3f9fa1901f22227e7d1ba0c4902b5bc50981
eeemsi <eeemsi@gmail.com>
2012-04-19 14:45:45 +0200

    the folder is now easier to set an argument can be given to clean the given git repository

--- update_cloned_git.sh
+++ update_cloned_git.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-ARRAY=`ls $HOME/cloned_git/`
+folder='cloned_git'
+ARRAY=`ls $HOME/$folder/`
 
 for i in $ARRAY; do
-	if [ -d "$HOME/cloned_git/$i" ]; then
-	cd $HOME/cloned_git/$i && make distclean;
-    cd $HOME/cloned_git/$i && git clean -f && git gc;
-	cd $HOME/cloned_git/$i && git pull;
+	if [ -d "$HOME/$folder/$i" ]; then
+        echo 'currently working in' $i;
+	    cd $HOME/$folder/$i && git pull;
+	    case $argv
+            in "clean")
+                echo 'cleaning up' $i;
+                cd $HOME/$folder/$i && make distclean;
+                cd $HOME/$folder/$i && git clean -f && git gc;
+            ;;
+        esac
 	fi
 done
