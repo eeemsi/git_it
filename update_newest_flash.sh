bb453d432958a0e5f3f1536b95d6f257d0e993d5
eeemsi <eeemsi@gmail.com>
2012-08-24 16:28:28 +0200

    added hint how to start chromium

--- update_newest_flash.sh
+++ update_newest_flash.sh
@@ -10,7 +10,12 @@ cdt () {
     echo "using the following directory to download: $t\n" && cd "$t"
 }
 
-cdt && wget https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_$target.deb && ar vx google-chrome-stable_current_$target.deb && tar xJf data.tar.lzma
+cdt &&
+wget https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_$target.deb &&
+ar vx google-chrome-stable_current_$target.deb &&
+tar xJf data.tar.lzma
 
 echo "\n\n=====================\nsteps left up to you:\nplease cp -r <the given directory>opt/google/chrome/PepperFlash /usr/lib"
 echo "ready to chmod 644 /usr/lib/PepperFlash/libpepflashplayer.so"
+echo "\n\n=====================\nplease start chromium the following way:\n --ppapi-flash-path=/usr/lib/PepperFlash/libpepflashplayer.so --ppapi-flash-version=<version>"
+
