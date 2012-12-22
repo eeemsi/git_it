b01b97fb66912ae471dcb3e538f19870e905de14
eeemsi <eeemsi@gmail.com>
2012-12-22 21:04:33 +0100

    readability enhancements

--- dyndns_checkr.sh
+++ dyndns_checkr.sh
@@ -7,10 +7,6 @@
 cip=`ifconfig eth0.1 | sed -n '/dr:/{;s/.*dr:\([0-9.]\+\) .*/\1/;p;}'`
 lip=`nslookup bl4.dyndns.org | tail -2 | grep Address | awk '{print $3}'`
 
-if [ $cip != $lip ];
-then
+if [ $cip != $lip ]; then
     /bin/update_dyndns.sh
-else
-    echo "good"
 fi
-exit 0
