aa2c473cf58538548780665b182381b161ab67cb
eeemsi <eeemsi@gmail.com>
2012-12-22 21:05:43 +0100

    added missing quotes

--- dyndns_checkr.sh
+++ dyndns_checkr.sh
@@ -7,6 +7,6 @@
 cip=`ifconfig eth0.1 | sed -n '/dr:/{;s/.*dr:\([0-9.]\+\) .*/\1/;p;}'`
 lip=`nslookup bl4.dyndns.org | tail -2 | grep Address | awk '{print $3}'`
 
-if [ $cip != $lip ]; then
+if [ "$cip" != "$lip" ]; then
     /bin/update_dyndns.sh
 fi
