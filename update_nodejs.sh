28a41f9de3d6b1a2835ad63e294456760b755296
eeemsi <eeemsi@gmail.com>
2013-02-26 18:04:43 +0100

    created a small shellscript to make updating nodejs a bit more comfortable

---
+++ update_nodejs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+version="${1}"
+
+make_tmp_dir() {
+    if [ ! -d /tmp/"$version"_nodejs ]; then
+        mkdir /tmp/"$version"_nodejs
+    fi
+
+    cd /tmp/"$version"_nodejs
+
+    get_nodejs_version
+}
+
+get_nodejs_version() {
+    if [ ! -f /tmp/"$version"_nodejs/node-v"$version".tar.gz ]; then
+        wget -c http://nodejs.org/dist/v"$version"/node-v"$version".tar.gz
+    fi
+
+    extract_build_install
+}
+
+extract_build_install() {
+    tar xf node-v"$version".tar.gz && cd node-v"$version"
+    ./configure --prefix=/opt/node --openssl-includes=/usr/include/openss && make
+    sudo make install clean
+
+    remove_created_temp_dir
+}
+
+remove_created_temp_dir() {
+    rm -rf /tmp/"$version"_nodejs
+}
+
+make_tmp_dir
