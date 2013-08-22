fc8907100e18107977805785f09bb48cd3e90512
eeemsi <eeemsi@gmail.com>
2013-08-22 20:45:58 +0200

    added check for version echo path if node was installed exit if no version is named

--- update_nodejs.sh
+++ update_nodejs.sh
@@ -2,6 +2,31 @@
 
 version="${1}"
 
+check_version() {
+    if [ ! -f "/opt/node/bin/node" ]; then
+        installed_version=""
+    else
+        installed_version="$(node -v | sed 's/^.//')"
+    fi
+
+    if [ -z "$installed_version" ]; then
+        echo_path
+    fi
+
+    if [ "$installed_version" != "$version" ]; then
+        make_tmp_dir
+    else
+        echo "requested version equals installed version"
+        exit
+    fi
+}
+
+echo_path() {
+    echo "# Set PATHs for node.js
+    export NODE_PATH=/opt/node:/opt/node/lib/node_modules
+    export PATH=$PATH:/opt/node/bin" > ${HOME}/.zsh/nodejs
+}
+
 make_tmp_dir() {
     if [ ! -d /tmp/nodejs_"$version" ]; then
         mkdir /tmp/nodejs_"$version"
@@ -43,4 +68,8 @@ remove_created_temp_dir() {
     rm -rf /tmp/nodejs_"$version"
 }
 
-make_tmp_dir
+if [ ! -z "${1}" ]; then
+    check_version
+else
+    return 1
+fi
