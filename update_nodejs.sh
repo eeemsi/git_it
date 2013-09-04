f4cbf0a0c902046eba3c4dc2e46bce144a892df9
eeemsi <eeemsi@gmail.com>
2013-09-04 12:39:08 +0200

    fixed ignoring set_path and some more issues retab

--- update_nodejs.sh
+++ update_nodejs.sh
@@ -3,71 +3,74 @@
 version="${1}"
 
 check_version() {
-    if [ ! -f "/opt/node/bin/node" ]; then
-        installed_version=""
-    else
-        installed_version="$(node -v | sed 's/^.//')"
-    fi
+	if [ -f "/opt/node/bin/node" ]; then
+		installed_version="$(node -v | sed 's/^.//')"
+	else
+		installed_version=""
+	fi
 
-    if [ -z "$installed_version" ]; then
-        echo_path
-    fi
-
-    if [ "$installed_version" != "$version" ]; then
-        make_tmp_dir
-    else
-        echo "requested version equals installed version"
-        exit
-    fi
-}
-
-echo_path() {
-    echo "# Set PATHs for node.js\nexport PATH="'$PATH'":/opt/node/bin" > ${HOME}/.zsh/nodejs
+	if [ "$installed_version" != "$version" ]; then
+		make_tmp_dir
+	else
+		echo "requested version equals installed version"
+		exit 0
+	fi
 }
 
 make_tmp_dir() {
-    if [ ! -d /tmp/nodejs_"$version" ]; then
-        mkdir /tmp/nodejs_"$version"
-    fi
+	if [ ! -d /tmp/nodejs_"$version" ]; then
+		mkdir /tmp/nodejs_"$version"
+	fi
 
-    cd /tmp/nodejs_"$version"
+	cd /tmp/nodejs_"$version"
 
-    get_nodejs_version
+	get_nodejs_version
 }
 
 get_nodejs_version() {
-    if [ ! -f node-v"$version".tar.gz ]; then
-        wget -c http://nodejs.org/dist/v"$version"/node-v"$version".tar.gz
-    fi
+	if [ ! -f node-v"$version".tar.gz ]; then
+		wget -c http://nodejs.org/dist/v"$version"/node-v"$version".tar.gz
+	fi
 
-    extract_build_install
+	extract_build_install
 }
 
 extract_build_install() {
-    if [ ! -d node-v"$version" ]; then
-        tar xf node-v"$version".tar.gz
-    fi
+	if [ ! -d node-v"$version" ]; then
+		tar xf node-v"$version".tar.gz
+	fi
 
-    cd node-v"$version"
+	cd node-v"$version"
 
-    if [ -x "$(which clang)" ]; then
-        export CC=clang
-        export CXX=clang++
-    fi
+	if [ -x "$(which clang)" ]; then
+		export CC=clang
+		export CXX=clang++
+	fi
 
-    ./configure --prefix=/opt/node --openssl-includes=/usr/include/openssl
+	./configure --prefix=/opt/node --openssl-includes=/usr/include/openssl
 
-    make && sudo make install clean
+	make && sudo make install clean
 
-    remove_created_temp_dir
+	remove_created_temp_dir
 }
 
 remove_created_temp_dir() {
-    rm -rf /tmp/nodejs_"$version"
+	if [ -d /tmp/nodejs_"$version" ]; then
+		rm -rf /tmp/nodejs_"$version"
+	fi
+}
+
+echo_path() {
+	if [ ! -f ${HOME}/.zsh/nodejs ]; then
+		echo "# Set PATHs for node.js\nexport PATH="'$PATH'":/opt/node/bin" > ${HOME}/.zsh/nodejs
+	fi
+
+	exit 0
 }
 
 if [ ! -z "${1}" ]; then
-    check_version
+	check_version
+	echo_path
 else
-    return 1
+	return 1
 fi
