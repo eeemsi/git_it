267fb1b2d8c197992e5885b578655e27396781f6
eeemsi <eeemsi@gmail.com>
2013-11-21 21:38:26 +0100

    make zsh stuff in deploy script more granular retab

--- deploy.sh
+++ deploy.sh
@@ -3,61 +3,75 @@
 cp=/bin/cp
 
 if [ ! -z "${1}" ]; then
-    case "${1}" in
-        i3)
-            if [ ! -d "${HOME}"/.i3 ]; then
-                mkdir "${HOME}"/.i3 && cp ./i3/config "${HOME}"/.i3/config
-            else
-                cp ./i3/config "${HOME}"/.i3/config
-            fi
-            ;;
+	case "${1}" in
+		i3)
+			if [ ! -d "${HOME}"/.i3 ]; then
+				mkdir "${HOME}"/.i3 && cp ./i3/config "${HOME}"/.i3/config
+			else
+				cp ./i3/config "${HOME}"/.i3/config
+			fi
+			;;
 
-        i3status)
-            cp ./i3/i3status.conf "${HOME}"/.i3/i3status.conf
-            ;;
+		i3status)
+			cp ./i3/i3status.conf "${HOME}"/.i3/i3status.conf
+			;;
 
-        vim)
-            cp ./vimrc "${HOME}"/.vimrc
+		vim)
+			cp ./vimrc "${HOME}"/.vimrc
 
-            if [ ! -d "${HOME}"/.vim ]; then
-                cp -r ./vim "${HOME}"/.vim
-            fi
-            ;;
+			if [ ! -d "${HOME}"/.vim ]; then
+				cp -r ./vim "${HOME}"/.vim
+			fi
+			;;
 
-        xresources)
-            cp ./Xresources "${HOME}"/.Xresources
-            ;;
+		xresources)
+			cp ./Xresources "${HOME}"/.Xresources
+			;;
 
-        zsh)
-            cp ./zshenv "${HOME}"/.zshenv
-            cp ./zprofile "${HOME}"/.zprofile
+		zsh)
+			cp ./zshenv "${HOME}"/.zshenv
+			cp ./zprofile "${HOME}"/.zprofile
 
-            if [ -d "${HOME}"/.zsh ]; then
-                cp ./zsh/* "${HOME}"/.zsh/
-            else
-                cp -r ./zsh "${HOME}"/.zsh
-            fi
+			if [ -d "${HOME}"/.zsh ]; then
+				cp ./zsh/* "${HOME}"/.zsh/
+			else
+				cp -r ./zsh "${HOME}"/.zsh
+			fi
 
-            if [ -f "${HOME}"/.zshrc ]; then
-                rm "${HOME}"/.zshrc
-            fi
+			if [ -f "${HOME}"/.zshrc ]; then
+				rm "${HOME}"/.zshrc
+			fi
 
-            if [ -x "$(which go)" ] && [ ! -f "${HOME}"/.zsh/golang ]; then
-                if [ ! -d "${HOME}"/golang ]; then
-                    mkdir "${HOME}"/golang
-                fi
+			for i in `ls "${HOME}"/.zsh`; do
+				echo "source \""'${HOME}'/.zsh/"${i}""\"" >> "${HOME}"/.zshrc
+			done
+			;;
 
-                echo "# Set gopath \nexport GOPATH="${HOME}"/golang" > "${HOME}"/.zsh/golang
-            fi
+		zsh-debian)
+			if [ ! -f "${HOME}"/.zsh/debian ]; then
+				cp zsh_debian/debian "${HOME}"/.zsh/debian
+			fi
 
-            for i in `ls "${HOME}"/.zsh`; do
-                echo "source \""'${HOME}'/.zsh/"${i}""\"" >> "${HOME}"/.zshrc
-            done
-            ;;
-        *)
-            echo "wrong argument"
-            ;;
-    esac
+			echo "source \""'${HOME}'/.zsh/debian"\"" >> "${HOME}"/.zshrc
+			;;
+
+		zsh-golang)
+			if [ -x "$(which go)" ] && [ ! -f "${HOME}"/.zsh/golang ]; then
+				if [ ! -d "${HOME}"/golang ]; then
+					mkdir "${HOME}"/golang
+				fi
+
+				echo "# Set gopath \nexport GOPATH="${HOME}"/golang" > "${HOME}"/.zsh/golang
+			fi
+
+			echo "source \""'${HOME}'/.zsh/golang"\"" >> "${HOME}"/.zshrc
+			;;
+		*)
+			echo "wrong argument"
+			exit 1
+			;;
+	esac
 else
-    echo "nothing specified"
+	echo "nothing specified"
+	exit 1
 fi
